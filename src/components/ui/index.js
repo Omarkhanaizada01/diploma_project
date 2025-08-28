a1650988ea4d230265ac429aee824689dec82e9e
Aizada <aizada.omarhan01@gmail.com>
2025-08-28 12:34:21 +0500

    fix: исправлен регистр в пути импорта Tag

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -4,4 +4,4 @@ export { default as ArrowButton } from './buttons/ArrowButton';
 export { default as ProductCard } from './cards/ProductCard/ProductCard';
 export { default as SaleCard } from './cards/ProductCard/variants/SaleCard';
 export { default as HoverCard } from './cards/ProductCard/variants/HoverCard';
-export { default as Tag } from './Tag/Tag';
+export { default as Tag } from './tag/Tag';
